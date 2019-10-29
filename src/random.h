fc27c4f8b8a65e94ba56814f86803142b3eea9a5
zkh <zhangkaihuo@cortexlabs.ai>
2019-10-29 15:30:53 +0800

    use blake2b and siphash24

--- src/random.h
+++ src/random.h
@@ -5,24 +5,41 @@
 #include <cstdlib>
 #include <stdint.h>
 #include <time.h>
+#include "siphash.h"
 
 
 namespace XCortex{
 
   class Random{
     uint32_t nonce;
+    siphash_keys sipkeys;
     public:
     Random(){
       srand((unsigned)time(0));
     }
+    Random(const siphash_keys& sipkeys){
+      this->sipkeys.k0 = sipkeys.k0;
+      this->sipkeys.k1 = sipkeys.k1;
+      this->sipkeys.k2 = sipkeys.k2;
+      this->sipkeys.k3 = sipkeys.k3;
+    }
+    void set_keys(const siphash_keys& sipkeys){
+      this->sipkeys.k0 = sipkeys.k0;
+      this->sipkeys.k1 = sipkeys.k1;
+      this->sipkeys.k2 = sipkeys.k2;
+      this->sipkeys.k3 = sipkeys.k3;
+    }
     int32_t generate_random_value(){
-      ++nonce;
-      return rand() % 255 - 127;
+      uint64_t r = siphash24(&sipkeys, nonce++);
+      return r % 255 - 127;
+      //return rand() % 255 - 127;
     }
     int32_t generate_random_value(const uint32_t min, const uint32_t max){
-      ++nonce;
+      //++nonce;
       const uint32_t size = max - min;
-      return rand() % size + min;
+      //return rand() % size + min;
+      uint64_t r = siphash24(&sipkeys, nonce++);
+      return r % size + min;
     }
     std::vector<int32_t> generate_random_value(const uint32_t n){
       std::vector<int32_t> out(n);
--- src/xcortex.cpp
+++ src/xcortex.cpp
@@ -1,6 +1,8 @@
 #include "dataset.h"
 #include "dense.hpp"
 #include "relu.hpp"
+#include "siphash.h"
+#include "blake2.h"
 
 #include <iostream>
 using namespace std;
@@ -8,22 +10,60 @@ using namespace XCortex;
 
 #define NUM_OPS 32
 
-void xcortex(vector<XCortex::OP*> ops, DataSet& data_set){
-  int num_ops = NUM_OPS;//xcortex_random.generate_random_value(MIN_OP, MAX_OP);
-  //random ops
-  vector<int32_t> op_ids(num_ops);
-  op_ids = xcortex_random.generate_random_value(num_ops, 0, ops.size()); 
-  //execute ops
-  for(int i = 0; i < num_ops; i++){
-    const int op_id = op_ids[i];
-    OP* op = ops[op_id];
-#ifdef DEBUG
-    cout << "*****op " << i << ": " << op->name << endl;
+namespace XCortex{
+  class XCortex{
+    siphash_keys sipkeys;
+    public:
+      XCortex(){
+      }
+      void setheader(const char *header, const u32 headerlen, siphash_keys *keys) {
+        char hdrkey[32];
+        // SHA256((unsigned char *)header, headerlen, (unsigned char *)hdrkey);
+        //  printf("call blake2b %d %d %d %d\n", hdrkey, sizeof(hdrkey), header, headerlen);
+        blake2b((void *)hdrkey, sizeof(hdrkey), (const void *)header, headerlen, 0, 0);
+        //  assert(0);
+#ifdef SIPHASH_COMPAT
+        u64 *k = (u64 *)hdrkey;
+        u64 k0 = k[0];
+        u64 k1 = k[1];
+        printf("k0 k1 %lx %lx\n", k0, k1);
+        k[0] = k0 ^ 0x736f6d6570736575ULL;
+        k[1] = k1 ^ 0x646f72616e646f6dULL;
+        k[2] = k0 ^ 0x6c7967656e657261ULL;
+        k[3] = k1 ^ 0x7465646279746573ULL;
 #endif
-    op->init(data_set);
-    op->run(data_set);
-  }
-}
+        setkeys(keys, hdrkey);
+      }
+      void set_header_nonce(std::vector<uint8_t> header, uint64_t nonce){
+        uint64_t littleEndianNonce = htole64(nonce);
+        char headerBuf[40];
+        memcpy(headerBuf, header.data(), 32);
+        memcpy(headerBuf + 32, static_cast<uint64_t*>(&littleEndianNonce), sizeof(nonce));
+        setheader(headerBuf, 40, &sipkeys);
+        xcortex_random.set_keys(sipkeys);
+      }
+      void run(vector<OP*> ops, DataSet& data_set){
+        int num_ops = NUM_OPS;//xcortex_random.generate_random_value(MIN_OP, MAX_OP);
+        //random ops
+        vector<int32_t> op_ids(num_ops);
+        op_ids = xcortex_random.generate_random_value(num_ops, 0, ops.size()); 
+        //execute ops
+        for(int i = 0; i < num_ops; i++){
+          const int op_id = op_ids[i];
+          OP* op = ops[op_id];
+#ifdef DEBUG
+          cout << "*****op " << i << ": " << op->name << endl;
+#endif
+          op->init(data_set);
+          op->run(data_set);
+        }
+      }
+  };
+
+};
+
+
+#define HEADERLEN 80
 int main(){
 //  vector<const Op*> ops = utils::Registry<Op>::List();
 //  for(int i = 0; i < ops.size(); i++){
@@ -33,12 +73,21 @@ int main(){
 //        << ", arguments: " << ops[i]->arguments.size()
 //        << endl;
 //  }
+  vector<uint8_t> header(HEADERLEN, 0);
+  uint64_t nonce = 0;
+
+  XCortex::XCortex xcortex; 
+  xcortex.set_header_nonce(header, 0);
+
   DataSet data_set(DATA_SET_SIZE);
   data_set.print();
+
   vector<XCortex::OP*> ops;
   ops.push_back(new Dense());
   ops.push_back(new Relu());
-  xcortex(ops, data_set);
+  
+  xcortex.run(ops, data_set);
+
   data_set.print();
   return 0;
 }
